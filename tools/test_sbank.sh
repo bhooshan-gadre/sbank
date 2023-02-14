a0243ebffef2bffb8d6782a2551920c3e624cff4
Bhooshan Gadre <bgadre@visar.nikhef.nl>
2023-02-14 18:10:20 +0100

    example to generate tidal bank

--- tools/test_sbank.sh
+++ tools/test_sbank.sh
@@ -22,3 +22,8 @@ if ((HDF_SIZE < 140 || HDF_SIZE > 160)); then
   exit 1
 fi
 
+# sbank --approximant IMRPhenomD_NRTidalv2 --aligned-spin --mass1-min 1.4 --mass1-max \
+# 1.5 --spin1-min 0.0 --spin1-max 0.2 --mass2-min 1.4 --mass2-max 1.5 \
+# --spin2-min 0.0 --spin2-max 0.0 --use-tidal --lambda1-only --lambda1-min 3000 --lambda2-max 5000 \
+# --match-min 0.97 --flow 30.0 --noise-model aLIGOZeroDetHighPower \
+# --output-filename NSBH-IMRPhenomD_NRTidal2-aLIGOZeroDetHighPower.hdf --verbose
